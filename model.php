d0df6d71fad3f7baa8832c9212263b1cfb319f9e
Pablo Pinero <pinerop@gmail.com>
2015-04-13 15:57:54 -0500

    model functions

--- model.php
+++ model.php
@@ -1,12 +1,16 @@
 <?php 
 
+define("DB_HOST", "127.0.0.1");
+define("DB_NAME", 'parks_db');
+define("DB_USER", "parks_user");
+define("DB_PASS", "codeup");
+
 class Model {
 
     protected static $dbc;
     protected static $table;
 
     public $attributes = array();
-
     /*
      * Constructor
      */
@@ -18,17 +22,14 @@ class Model {
     /*
      * Connect to the DB
      */
-    private static function dbConnect()
+    static function dbConnect()
     {
         if (!self::$dbc)
         {
-            define("DB_HOST", "127.0.0.1");
-			define("DB_NAME", 'national_parks_db');
-			define("DB_USER", "parks_user");
-			define("DB_PASS", "codeup");
 
-			$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME , DB_USER , DB_PASS);
-$			dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			self::$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+			self::$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			echo self::$dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS);
         }
     }
 
@@ -60,16 +61,37 @@ $			dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     public function save()
     {
         // @TODO: Ensure there are attributes before attempting to save
-
         // @TODO: Perform the proper action - if the `id` is set, this is an update, if not it is a insert
-
         // @TODO: Ensure that update is properly handled with the id key
-
         // @TODO: After insert, add the id back to the attributes array so the object can properly reflect the id
-
         // @TODO: You will need to iterate through all the attributes to build the prepared query
-
         // @TODO: Use prepared statements to ensure data security
+    	if (isset($this->attributes)){
+    		$stmt = $this->dbc->prepare("SELECT * FROM " . static::$table . " WHERE id= :id");
+    		$stmt->bindValue(":table", $static->table, PDO::PARAM_STR);
+    		$stmt->bindValue(":id", $this->attributes["id"], PDO::PARAM_INT);
+    		
+    		$results = 	$stmt->execute();
+
+    		if($results){
+    			foreach ($attributes as $key => $value) {
+    				if ($key != 'id'){
+    					$stmt = $this->dbc->prepare("UPDATE :table SET :key = :value");
+
+    					$stmt->bindValue(":table", $static->table, PDO::PARAM_STR);
+    					$stmt->bindValue(":key", $key, PDO::PARAM_STR);
+    					$stmt->bindValue(":value", $value, PDO::PARAM_INT);
+
+    					$stmt->execute();
+    				}
+    			}
+    		} else {
+
+
+    		}
+    	}
+    }
+     
 
     /*
      * Find a record based on an id
@@ -78,11 +100,13 @@ $			dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     {
         // Get connection to the database
         self::dbConnect();
-
         // @TODO: Create select statement using prepared statements
-
+        $stmt = self::$dbc->prepare("SELECT * FROM " . static::$table . " WHERE id= :id");
+        $stmt->bindValue(":table", self::$table, PDO::PARAM_STR);
+    	$stmt->bindValue(":id", $id, PDO::PARAM_INT);
+    	$stmt->execute();
         // @TODO: Store the resultset in a variable named $result
-
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
         // The following code will set the attributes on the calling object based on the result variable's contents
 
         $instance = null;
@@ -100,10 +124,13 @@ $			dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     public static function all()
     {
         self::dbConnect();
-
         // @TODO: Learning from the previous method, return all the matching records
+        return self::$dbc->query("SELECT * FROM " . static::$table . ';')->fetchAll(PDO::FETCH_ASSOC);
     }
 
 }
+
+$test = new Model;
+$test->dbConnect();
 
 ?>
